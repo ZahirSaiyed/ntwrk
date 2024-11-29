c5308d0a800c7a5124b5a55ebc742621b0603f4b
Zahir Saiyed <zahir19saiyed@gmail.com>
2024-11-28 22:19:24 -0500

    Made fix to ensure route files  only export HTTP method handlers

--- src/app/api/contacts/[email]/route.ts
+++ src/app/api/contacts/[email]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { Contact } from '@/types';
 
 export async function PUT(
